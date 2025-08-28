6fc951ac40629dd103ccfa8b427213abd0b227b7
GuiFcosta <gbigui0206@gmail.com>
2025-08-28 10:19:35 -0300

    Refactor commit

--- rest-with-spring-boot-and-java-erudio/src/main/java/pt/com/GuiFcosta/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/pt/com/GuiFcosta/controllers/MathController.java
@@ -1,4 +1,30 @@
 package pt.com.GuiFcosta.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/math")
 public class MathController {
+    // http://localhost:8080/math/sum/3/5
+    @RequestMapping("/sum/{a}/{b}")
+    public Double sum(@PathVariable("a") String a, @PathVariable("b") String b) throws Exception{
+        if(isNumeric(a) || isNumeric(b)){
+            throw  new Exception("Invalid input");
+        }
+        return convertToDouble(a) + convertToDouble(b);
+    }
+
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
+        if (strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
+    }
+
+    public static boolean isNumeric(String strNumber){
+        if (strNumber == null || strNumber.isEmpty()) return true;
+        String number = strNumber.replace(",", ".");
+        return !number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
 }
